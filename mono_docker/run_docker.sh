abfa3c9992bccfd203231d4bfce2739f4f7530e3
Daniel-Salvador <dvccs@softex.cin.ufpe.br>
2024-10-25 10:36:23 -0300

    ADD: Docker files

---
+++ mono_docker/run_docker.sh
@@ -0,0 +1,14 @@
+xhost si:localuser:root
+
+docker run -it \
+           --privileged \
+           --name mono_ros \
+           --net=host \
+           --env="DISPLAY" \
+           --volume="/home/dvccs/dev/Monografia_RRIA/catkin_ws:/catkin_ws:rw" \
+           --volume="/home/dvccs/dev/Monografia_RRIA/champ_ws:/champ_ws:rw" \
+	   --gpus all \
+           --env="QT_X11_NO_MITSHM=1" \
+           mono_ros \
+           bash
+
---
+++ mono_docker/start_docker.sh
@@ -0,0 +1,4 @@
+xhost si:localuser:root
+
+docker start mono_ros
+docker attach mono_ros
