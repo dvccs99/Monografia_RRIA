4f6b206630705c710828e78a37d6198aca6476f9
Daniel-Salvador <dvccs@softex.cin.ufpe.br>
2024-10-27 01:57:16 -0300

    ADD(quad_robot.py): Creating Gym environment

---
+++ champ_ws/src/champ_pkg/src/quad_robot.py
@@ -0,0 +1,29 @@
+import gymnasium as gym
+from gymnasium import spaces
+
+
+class QuadRobotEnv(gym.env):
+    metadata = {"render_modes": ["human", "other"], "render_fps": 60}
+
+    def __init__(self, render_mode=None):
+        self.observation_space 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("funfou")
